5d89d7f1ca6a17139e22a3c211c9baaef49b23cb
ariannacostanzo <costanzo.arianna@outlook.it>
2024-05-27 11:49:24 +0200

    refactoring

--- src/org/events/Main.java
+++ src/org/events/Main.java
@@ -9,11 +9,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-
         Scanner scanner = new Scanner(System.in);
-        LocalDate date = null;
-        int seatingCapacity = 0;
         Event event = null;
 
         //inizio
@@ -23,29 +19,14 @@ public class Main {
         //finchè non creo l'evento continuo a chiedere titolo, data e capienza location
         while (event == null) {
             try {
+                //ottengo titolo, data e capacità
                 System.out.println("Inserisci il titolo dell'evento");
                 String title = scanner.nextLine();
-                //se il formato della data non è valido lo richiedo
-                do {
-                    try {
-                        System.out.println("Inserisci la data (yyyy-MM-dd)");
-                        String dateString = scanner.nextLine();
-                        date = LocalDate.parse(dateString);
-                    } catch (EventException | DateTimeParseException e) {
-                        System.out.println("!!! Formato data non valido !!!");
-                    }
-                }
-                while (date == null);
-                //se il formato numero della capienza non è valido lo richiedo
-                do {
-                    try {
-                        System.out.println("Inserisci la capacità dei posti della location");
-                        seatingCapacity = Integer.parseInt(scanner.nextLine());
-                    } catch (NumberFormatException e) {
-                        System.out.println("!!! Formato numero non valido !!!");;
-                    }
-                }
-                while (seatingCapacity == 0);
+
+                LocalDate date = getDateFromUser(scanner);
+                int seatingCapacity = getSeatingCapacityFromUser(scanner);
+
+                //inizializzo l'evento
                 event = new Event(title, date, seatingCapacity);
                 //se qualche campo è errato mostro l'errore e ricomincio da capo
             } catch (EventException e) {
@@ -55,11 +36,9 @@ public class Main {
         }
 
         System.out.println("Hai creato questo evento: " + event);
-
-        //menù
-
         boolean run = true;
 
+        //menù
         while(run) {
             System.out.println("--------------------------------------------");
             System.out.println("Il tuo evento: " + event);
@@ -71,50 +50,85 @@ public class Main {
             System.out.println("3 - Exit");
             String choice = scanner.nextLine();
             switch (choice) {
-                //prenoto dei posti se il numero è valido e se non è maggiore della capienza
                 case "1":
-                    int seatsToBook = 0;
-                    do {
-                        try {
-                            System.out.println("Quanti posti vuoi prenotare?");
-                            seatsToBook = Integer.parseInt(scanner.nextLine());
-                        } catch (NumberFormatException e) {
-                            System.out.println("!!! Formato numero invalido !!!");;
-                        }
-                    } while(seatsToBook == 0);
-                    try {
-                        event.book(seatsToBook);
-                        System.out.println("Prenotazione avvenuta con successo. Hai prenotato: " + seatsToBook + " posti");
-                    } catch (EventException e) {
-                        System.out.println(e.getMessage());
-                    }
+                    book(scanner, event);
                     break;
-                    //disdico dei posti se il numero è valido ed è minore della capienza e minore dei posti prenotati
                 case "2":
-                    int seatsToCancel = 0;
-                    do {
-                        try {
-                            System.out.println("Quanti posti vuoi disdire?");
-                            seatsToCancel = Integer.parseInt(scanner.nextLine());
-                        } catch (NumberFormatException e) {
-                            System.out.println("!!! Formato numero invalido !!!");;
-                        }
-                    } while (seatsToCancel == 0);
-                    try {
-                        event.cancel(seatsToCancel);
-                        System.out.println("Cancellazione avvenuta con successo. Hai disdetto: " + seatsToCancel + " posti");
-                    } catch (EventException e) {
-                        System.out.println(e.getMessage());
-                    }
+                    cancel(scanner, event);
                     break;
-                    //esco se non premo 1 o 2
+                //esco se non premo 1 o 2
                 default:
                     run = false;
                     break;
             }
-
         }
-
         scanner.close();
+    }
+
+    //se il formato della data non è valido lo richiedo
+    private static LocalDate getDateFromUser(Scanner scanner) {
+        LocalDate date = null;
+        while (date == null) {
+            try {
+                System.out.println("Inserisci la data (yyyy-MM-dd)");
+                String dateString = scanner.nextLine();
+                date = LocalDate.parse(dateString);
+            } catch (DateTimeParseException e) {
+                System.out.println("!!! Formato data non valido !!!");
+            }
+        }
+        return date;
+    }
+
+    //se il formato numero della capienza non è valido lo richiedo
+    private static int getSeatingCapacityFromUser(Scanner scanner) {
+        int seatingCapacity = 0;
+        while (seatingCapacity == 0) {
+            try {
+                System.out.println("Inserisci la capacità dei posti della location");
+                seatingCapacity = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("!!! Formato numero non valido !!!");
+            }
+        }
+        return seatingCapacity;
+    }
+
+    //prenoto dei posti se il numero è valido e se non è maggiore della capienza
+    private static void book(Scanner scanner, Event event) {
+        int seatsToBook = 0;
+        do {
+            try {
+                System.out.println("Quanti posti vuoi prenotare?");
+                seatsToBook = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("!!! Formato numero invalido !!!");;
+            }
+        } while(seatsToBook == 0);
+        try {
+            event.book(seatsToBook);
+            System.out.println("Prenotazione avvenuta con successo. Hai prenotato: " + seatsToBook + " posti");
+        } catch (EventException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //disdico dei posti se il numero è valido ed è minore della capienza e minore dei posti prenotati
+    private static void cancel(Scanner scanner, Event event) {
+        int seatsToCancel = 0;
+        do {
+            try {
+                System.out.println("Quanti posti vuoi disdire?");
+                seatsToCancel = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("!!! Formato numero invalido !!!");;
+            }
+        } while (seatsToCancel == 0);
+        try {
+            event.cancel(seatsToCancel);
+            System.out.println("Cancellazione avvenuta con successo. Hai disdetto: " + seatsToCancel + " posti");
+        } catch (EventException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
